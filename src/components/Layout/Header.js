11f3b2194df3d7966d17329fe8167258da8a0d2d
wangyanqin <wangyanqin@zhihu.com>
2018-07-23 23:01:20 +0800

    test deploy

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,12 @@ import styles from './header.less'
 
 const MyHeader = props => {
   const {location: {pathname}} = props
-  const currentMenu = R.propOr('talents', '1', pathname.split('/')) || 'talents'
+  const path = pathname.split('/')
+  const currentMenu =
+    path.length > 1 && path[1] === 'ent'
+      ? R.propOr('talents', '2', path) || 'talents'
+      : R.propOr('talents', '1', path) || 'talents'
+  const prefix = path.length > 1 && path[1] === 'ent' ? '/ent' : ''
   return (
     <header className={styles.header}>
       <Menu
@@ -18,17 +23,17 @@ const MyHeader = props => {
         style={{lineHeight: '64px'}}
       >
         <Menu.Item key="logo">
-          <Link to="/talents" activeclassname="active">
+          <Link to={`${prefix}/talents`} activeclassname="active">
             <img className={styles.logo} src={logoUrl} alt="logo" />
           </Link>
         </Menu.Item>
         <Menu.Item key="talents">
-          <Link to="/talents" activeclassname="active">
+          <Link to={`${prefix}/talents`} activeclassname="active">
             搜索人才
           </Link>
         </Menu.Item>
         <Menu.Item key="resumes">
-          <Link to="/resumes" activeclassname="active">
+          <Link to={`${prefix}/resumes`} activeclassname="active">
             简历筛选
           </Link>
         </Menu.Item>
