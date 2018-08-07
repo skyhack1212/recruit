4326dbde8a433c5696e2ecb63a74386b54dbe00f
jiangwei <jw198x@gmail.com>
2018-08-07 21:47:14 +0800

    hotfix change all SALARY_OPTIONS type

--- src/constants/position.js
+++ src/constants/position.js
@@ -43,34 +43,34 @@ export const DEGREE_OPTIONS = [
 export const SALARY_OPTIONS = [
   {
     label: '3000及以下/月',
-    key: '7',
+    key: 7,
   },
   {
     label: '3000 ~ 7000/月',
-    key: '7',
+    key: 7,
   },
   {
     label: '7000 ~ 15000/月',
-    key: '7',
+    key: 7,
   },
   {
     label: '10000 ~ 20000/月',
-    key: '7',
+    key: 7,
   },
   {
     label: '15000 ~ 30000/月',
-    key: '7',
+    key: 7,
   },
   {
     label: '30000 ~ 50000/月',
-    key: '7',
+    key: 7,
   },
   {
     label: '50000以上/月',
-    key: '7',
+    key: 7,
   },
   {
     label: '自定义',
-    key: '7',
+    key: 7,
   },
 ]
